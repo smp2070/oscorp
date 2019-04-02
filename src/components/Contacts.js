cfcd7732355fb7565d9f3c60389859bc1cc1cd19
smp2070 <shvepsik7@gmail.com>
2019-04-02 18:53:22 +0300

    fix bugs

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -58,7 +58,7 @@ class Contacts extends Component {
 
   handleValidation = e => {
     const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // const emailRegExp = / /;
+    // const emailRegExp = / /; 
     const validation = {...this.state.validation}
     e = e.target
     switch (e.name) {
